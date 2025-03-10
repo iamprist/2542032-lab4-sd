b4ac3c34467f1df9308990a4e3298bd065735db7
iamprist <102292590+iamprist@users.noreply.github.com>
2025-03-10 16:18:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,77 @@
 //for our onclick button - the following must happen
 document.getElementById("submitButton").addEventListener("click", () => {
     let countryName = document.getElementById("CountryName").value.trim();
-    if (!countryName) return alert("Please enter a valid country name");
+
+    if (!countryName) {
+        alert("Please enter a valid country name");
+        return;
+    }
 
     fetchCountryData(countryName);
 });
 
-// Fetching the country details
-const fetchCountryData = (country) => {
-    fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true&fields=name,capital,population,region,flags,borders`)
-        .then(res => res.ok ? res.json() : Promise.reject("Oops! No results found for the entered country"))
-        .then(data => displayCountryInfo(data[0]))
-        .catch(err => showError(err));
-};
 
-// Display country details
-const displayCountryInfo = (country) => {
-    document.getElementById("country-info").innerHTML = `
-        <h1>${country.name.common}</h1>
-        <p><b>Capital:</b> ${country.capital?.[0] || "N/A"}</p>
-        <p><b>Population:</b> ${country.population.toLocaleString()}</p>
-        <p><b>Region:</b> ${country.region}</p>
-        <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
-    `;
 
-    // Fetching bordering countries (if available)
-    country.borders?.length ? fetchBorderCountries(country.borders) : showNoBorders();
-};
+function fetchCountryData(country) {
+    let myAPIURL = `https://restcountries.com/v3.1/name/${country}?fullText=true&fields=name,capital,population,region,flags,borders`;
 
-// Fetch and display the neighbouring countries
-const fetchBorderCountries = (borders) => {
-    fetch(`https://restcountries.com/v3.1/alpha?codes=${borders.join(",")}&fields=name,flags`)
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("bordering-countries").innerHTML = `
-                <h3>Bordering Countries:</h3>
-                ${data.map(c => `<section><p>${c.name.common}</p><img src="${c.flags.png}" alt="Flag of ${c.name.common}"></section>`).join("")}
-            `;
+    fetch(myAPIURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Oops! No results found for the entered Country");
+            }
+            return response.json();
         })
-        .catch(() => showNoBorders());
-};
+        .then(data => {
+            let countryData = data[0];
 
-// Handle errors
-const showError = (msg) => {
-    document.getElementById("country-info").innerHTML = `<p style="color:red;">${msg}</p>`;
-    document.getElementById("bordering-countries").innerHTML = "";
-};
+            // here we get the relevant country details that are needed for this lab
+            let countryInfo = `
+                <h1>${countryData.name.common}</h1
+                <p><b>Capital:</b> ${countryData.capital ? countryData.capital[0] : "N/A"}</p>
+                <p><b>Population:</b> ${countryData.population.toLocaleString()}</p>
+                <p><b>Region:</b> ${countryData.region}</p>
+                <img src="${countryData.flags.png}" alt="Flag of ${countryData.name.common}">
+            `;
 
-// Handle cases where a country has no neighbouring countries
-const showNoBorders = () => {
-    document.getElementById("bordering-countries").innerHTML = "<p>No neighbouring countries found.</p>";
-};
+            document.getElementById("country-info").innerHTML = countryInfo;
+
+            // Fetch and display all neighbouring countries , if none - make sure that the error is handled
+            if (countryData.borders && countryData.borders.length > 0) {
+                fetchBorderCountries(countryData.borders);
+            } else {
+                document.getElementById("bordering-countries").innerHTML = "<p>No bordering countries found.</p>";
+            }
+        })
+        .catch(error => {
+            document.getElementById("country-info").innerHTML = `<p style="color:red;">${error.message}</p>`;
+            document.getElementById("bordering-countries").innerHTML = "";
+        });
+}
+
+
+
+function fetchBorderCountries(borders) {
+    let myAPIURL = `https://restcountries.com/v3.1/alpha?codes=${borders.join(",")}&fields=name,flags`;
+
+    fetch(myAPIURL)
+        .then(response => response.json())
+        .then(data => {
+            let borderInfo = "<h3>Bordering Countries:</h3>";
+            data.forEach(country => {
+                borderInfo += `
+                    <div>
+                        <p>${country.name.common}</p>
+                        <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
+                        
+                    </div>
+                `;
+            });
+
+            document.getElementById("bordering-countries").innerHTML = borderInfo;
+        })
+        .catch(error => {
+            document.getElementById("bordering-countries").innerHTML = `<p style="color:red;">Could not fetch bordering countries.</p>`;
+        });
+}
 
